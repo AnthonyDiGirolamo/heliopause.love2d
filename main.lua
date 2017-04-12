190ef5c1fb89ad8a488292dc718c4afd67143cfd
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-04-12 00:25:50 -0700

    updated

--- main.lua
+++ main.lua
@@ -66,30 +66,6 @@ function rectfill(x1, y1, x2, y2, c)
   love.graphics.rectangle("fill", x1, y1, w, h)
 end
 
--- function circfill(x, y, radius, c)
---   color(c)
---   love.graphics.circle("fill", x, y, radius)
--- end
-
--- picocolors = {
---   {0x1D, 0x2B, 0x53}, -- 1 dark_blue
---   {0x7E, 0x25, 0x53}, -- 2 dark_purple
---   {0x00, 0x87, 0x51}, -- 3 dark_green
---   {0xAB, 0x52, 0x36}, -- 4 brown
---   {0x5F, 0x57, 0x4F}, -- 5 dark_gray
---   {0xC2, 0xC3, 0xC7}, -- 6 light_gray
---   {0xFF, 0xF1, 0xE8}, -- 7 white
---   {0xFF, 0x00, 0x4D}, -- 8 red
---   {0xFF, 0xA3, 0x00}, -- 9 orange
---   {0xFF, 0xEC, 0x27}, -- 10 yellow
---   {0x00, 0xE4, 0x36}, -- 11 green
---   {0x29, 0xAD, 0xFF}, -- 12 blue
---   {0x83, 0x76, 0x9C}, -- 13 indigo
---   {0xFF, 0x77, 0xA8}, -- 14 pink
---   {0xFF, 0xCC, 0xAA}, -- 15 peach
--- }
--- picocolors[0] = {0,0,0} -- 0 black
-
 -- black       = 0
 -- dark_blue   = 1
 -- dark_purple = 2
@@ -107,19 +83,9 @@ end
 -- pink        = 14
 -- peach       = 15
 
-
-
-
 function vector_distance(a,b)
   return (b-a):scaled_length()
 end
-
--- love.graphics.setBackgroundColor(255,255,255)
--- love.graphics.setColor( red, green, blue, alpha )
--- love.graphics.setLine(2, "smooth")
--- love.graphics.line(15, 25, 69, 89)
-
--- canvas = love.graphics.newCanvas( width, height, format, msaa )
 
 local time = 0
 local screen_width, screen_height = 1366, 768
